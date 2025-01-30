16d387a85708d6030d9691d2198df72800f7b1f6
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-29 18:43:26 -0600

    Create Operation Interfaz

---
+++ InterfazQ12025/IOperacion.cs
@@ -0,0 +1,11 @@
+﻿
+namespace InterfazQ12025
+{
+    public interface IOperacion
+    {
+        public double Suma();
+        public double Resta();
+        public double Multiplicacion();
+        public double Division();
+    }
+}
